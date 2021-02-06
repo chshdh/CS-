883eae684000160aa76c6d69c1ca07efacc774ed
chengshuo <1132050920@qq.com>
2021-02-06 14:16:18 +0800

    Create 零的排序.cpp

---
+++ 零的排序.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+	
+
+int main ()
+{
+	int a[] = { 0,1,2,3,4,5,0,3,0 };
+	
+	int j = 0;
+	for (int  i = 0; i < 9; i++) {
+		if (a[i] != 0) {
+			a[j] = a[i];
+			if (i != j)
+				a[i] = 0;
+			j++;
+		}
+	}
+	
+	for (int i = 0; i < 9; i++) {
+		cout << a[i] << endl;
+	}
+}
+
+
